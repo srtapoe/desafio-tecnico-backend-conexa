de0cf3e875d3e0dbecb47d7602d94bd2ff8ac76c
Joanice Oliveira <jhoaniceoliveira@gmail.com>
2024-12-19 18:33:38 -0300

    Alterados arquivos de dependências e config

--- plataforma-medicos-api/plataforma-medicos-api/src/main/resources/db/migrations/V1__create_table_medicos.sql
+++ plataforma-medicos-api/plataforma-medicos-api/src/main/resources/db/migrations/V1__create_table_medicos.sql
@@ -2,7 +2,7 @@ CREATE TABLE medicos
 (
     id              BIGINT AUTO_INCREMENT PRIMARY KEY,
     email           VARCHAR(255) UNIQUE NOT NULL,
-    password        VARCHAR(255)        NOT NULL,
+    senha           VARCHAR(255)        NOT NULL,
     especialidade   VARCHAR(50)         NOT NULL,
     cpf             VARCHAR(14) UNIQUE  NOT NULL,
     telefone        VARCHAR(20),
